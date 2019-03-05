5ae8f65fe05debb198049b5c1ef13131916ce6be
Matt Jarrett <cujarrett@gmail.com>
2019-03-04 22:44:02 -0600

    Adds more skills

--- js/skills-bar.js
+++ js/skills-bar.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function(){
   jQuery('.skillbar').each(function(){
     jQuery(this).find('.skillbar-bar').animate({
       width:jQuery(this).attr('data-percent')
-    },3000);
+    },6000);
   });
 });
