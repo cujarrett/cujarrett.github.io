c58847f34f5a56c3a288fd6e182f52f7c84f29b5
Matt Jarrett <cujarrett@gmail.com>
2019-03-06 00:28:45 -0600

    Completes about section

--- js/skills-bar.js
+++ js/skills-bar.js
@@ -1,7 +1,32 @@
-jQuery(document).ready(function(){
-  jQuery('.skillbar').each(function(){
-    jQuery(this).find('.skillbar-bar').animate({
-      width:jQuery(this).attr('data-percent')
-    },6000);
+$.fn.isOnScreen = function(){
+
+  var win = $(window);
+
+  var viewport = {
+    top : win.scrollTop(),
+    left : win.scrollLeft()
+  };
+  viewport.right = viewport.left + win.width();
+  viewport.bottom = viewport.top + win.height();
+
+  var bounds = this.offset();
+  bounds.right = bounds.left + this.outerWidth();
+  bounds.bottom = bounds.top + this.outerHeight();
+
+  return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if ($('#about').isOnScreen()) {
+      jQuery(document).ready(function(){
+        jQuery('.skillbar').each(function(){
+          jQuery(this).find('.skillbar-bar').animate({
+            width:jQuery(this).attr('data-percent')
+          },6000);
+        });
+      });
+    }
   });
 });
